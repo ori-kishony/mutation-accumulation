0959b1a0f23587b67454ee496a1d17de0edd7154
orik <iro1998@gmail.com>
2023-01-11 22:32:58 +0200

    add cell with fitness

--- cell.py
+++ cell.py
@@ -1,3 +1,4 @@
+from typing import Callable, Optional
 import numpy as np
 from numpy.random import default_rng
 
@@ -32,11 +33,39 @@ class Cell(object):
         return cells
 
 
+class FitnessCell(Cell):
+    def __init__(self, dna_sequence: np.ndarray, mutation_rate: float, n_bases=Cell.N_BASES,
+                 ref_dna_sequence: Optional[np.ndarray] = None):
+        super().__init__(dna_sequence, mutation_rate, n_bases=n_bases)
+        if ref_dna_sequence is None:
+            ref_dna_sequence = np.arange(len(dna_sequence)) % n_bases
+        self.ref_dna_sequence = ref_dna_sequence
+
+    @classmethod
+    def genetic_fitness(cls, cell):
+        return cls.genetic_similarity_fitness(cell.dna_sequence, cell.ref_dna_sequence)
+
+    @staticmethod
+    def genetic_similarity_fitness(dna_sequence1: np.ndarray, dna_sequence2: np.ndarray,
+                                   base_fitness: float = 2.0, fitness_boost: float = 1.0):
+        similarity = (dna_sequence1 == dna_sequence2).sum() / len(dna_sequence1)
+        fitness = base_fitness + similarity * fitness_boost
+        return fitness
+
+    def divide(self, fitness_function: Optional[Callable] = None):
+        if fitness_function is None:
+            fitness = self.genetic_fitness(self)
+        else:
+            fitness = fitness_function(self)
+        n_offspring = rng.choice(np.arange(int(fitness), int(fitness) + 2), p=[1 - (fitness % 1), fitness % 1])
+        return super().divide(n_offspring=n_offspring)
+
+
 def main():
     DNA_SIZE = int(1e6)
     mutation_rate = 3e-6
     dna_sequence = rng.choice(Cell.N_BASES, DNA_SIZE)
-    cell = Cell(dna_sequence, mutation_rate)
+    cell = FitnessCell(dna_sequence, mutation_rate)
     children = cell.divide()
     for n, child in enumerate(children):
         n_mutations = (child.dna_sequence != cell.dna_sequence).sum()
